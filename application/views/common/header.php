b7f183777986988a7a9c45dee97783fbcba5f02d
Marta Fernandes <marta.a.fernandes@gmail.com>
2011-12-28 17:55:55 +0000

    login

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,7 +1,7 @@
   <div id="header">
-    <h1><a href="/">Tribble</a></h1>
-  </div>
-  <div id="login">
-  
-  
+    <h1><a href="/">Tribble</a></h1>  
+  <ul id="login">
+  <li class="sign">Sign In</li>
+  <li class="log">Log In</li>
+  </ul>
   </div>
