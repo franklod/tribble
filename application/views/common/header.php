7835c510457ad47b6189fbb8016fb634b699284a
Marta Fernandes <marta.a.fernandes@gmail.com>
2011-12-29 11:37:32 +0000

    alteracao div main

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,7 +1,8 @@
   <div id="header">
     <h1><a href="/">Tribble</a></h1>  
   <ul id="login">
-  <li class="sign">Sign In</li>
-  <li class="log">Log In</li>
+  <li class="sign"><a href="/">Sign In</a></li>
+  <li class="log"><a href="/">Log In</a></li>
   </ul>
   </div>
+  <div id="main">
