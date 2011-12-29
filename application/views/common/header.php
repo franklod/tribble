e451362fbecddc35207aa90992b235ab318f56b2
Marta Fernandes <marta.a.fernandes@gmail.com>
2011-12-29 13:51:02 +0000

    nothing special

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,8 +1,8 @@
-  <div id="header">
-    <h1><a href="/">Tribble</a></h1>  
+<div id="header">
+  <h1><a href="/">Tribble</a></h1>
   <ul id="login">
-  <li class="sign"><a href="/">Sign In</a></li>
-  <li class="log"><a href="/">Log In</a></li>
+    <li class="sign"><a href="/">Sign In</a></li>
+    <li class="log btn"><a href="/">Log In</a></li>
   </ul>
-  </div>
-  <div id="main">
+</div>
+<div id="main">
