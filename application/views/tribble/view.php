1523bf42210a1fb46cc1d2a5403ac6abe428c326
Marta Fernandes <marta.a.fernandes@gmail.com>
2011-12-30 18:44:09 +0000

    tanta coisa!

--- application/views/tribble/view.php
+++ application/views/tribble/view.php
@@ -1,20 +1,46 @@
-<h1><?=$tribble->title?></h1>
-<p><?=$tribble->text?></p>
-<p><img src="<?=$tribble->image?>" /></p>
-<p><?=strftime('%B %d, %Y',mysql_to_unix($tribble->ts));?></p>
-<p>likes: <?=$tribble->likes?></p>
-<ul>
-  <? $tags = explode(',',$tribble->tags); ?>
-  <?foreach($tags as $tag):?>
-  <li><a href="#"><?=$tag?></a></li>  
-  <?endforeach?>  
-</ul>
-<ul>
-  <?foreach(json_decode($tribble->palette) as $color):?>
-    <li style="background: #<?=$color?>;">&nbsp;</li>
-  <?endforeach;?>
-</ul>
-<?if(!$replies):?>
-<h4>It seems no one has replied to this tribble</h4>
-<p>Beat them to the punch!</p>
-<?endif;?>
+<div class="tribble-container" >
+  <div class="tribble-user-info">
+    <p><span><img src="" width="42" height="42" /> </span><a href="/">user name</a></p>
+  </div>
+  <div class="tribble-img-container box"> <img src="<?=$tribble->image?>" /></div>
+  <div class="tribble-img-data">
+    <h2>
+      <?=$tribble->title?>
+    </h2>
+    <div class="tribble-date">
+      <?=strftime('%B %d, %Y',mysql_to_unix($tribble->ts));?>
+    </div>
+    <div class="tribble-desc">
+      <?=$tribble->text?>
+    </div>
+    <div class="tribble-tools">
+      <p class="ico"><a href="" class="likes">
+        <?=$tribble->likes?></a>likes
+      </p>
+    </div>
+    <h3>Tags</h3>
+    <ul class="tags">
+      <? $tags = explode(',',$tribble->tags); ?>
+      <?foreach($tags as $tag):?>
+      <li><a href="#">
+        <?=$tag?>
+        </a></li>
+      <?endforeach?>
+    </ul>
+    <h3>Color Scheme</h3>
+    <ul class="color-scheme">
+      <?foreach(json_decode($tribble->palette) as $color):?>
+      <li style="background: #<?=$color?>;">&nbsp;</li>
+      <?endforeach;?>
+    </ul>
+  </div>
+  <div class="comments-list">
+  <h3>It seems no one has replied to this tribble</h3>
+  <ul>
+  <?if(!$replies):?>
+<li>
+</li>
+  <?endif;?>
+  </ul>
+  </div>
+</div>
