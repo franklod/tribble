bbaf18fe17d8feee340ab46bc4af7696e70ff979
Pedro Correia <pedro.a.correia@co.sapo.pt>
2012-01-02 13:49:26 +0000

    cleanup

--- system/helpers/image_helper.php
+++ system/helpers/image_helper.php
@@ -40,124 +40,57 @@
  */
 if ( ! function_exists('getImageColorPalette'))
 {
-//  function getImageColorPalette($imageFile, $numColors = 8, $granularity = 5) 
-//  { 
-//     $granularity = max(1, abs((int)$granularity)); 
-//     $colors = array(); 
-//     $size = @getimagesize($imageFile); 
-//     if($size === false) 
-//     { 
-//        user_error("Unable to get image size data"); 
-//        return false; 
-//     }
-//     
-//     switch($size['mime']){
-//        case 'image/png':
-//          $img = @imagecreatefrompng($imageFile);
-//          break;
-//        case 'image/jpeg':
-//          $img = @imagecreatefromjpeg($imageFile);
-//          break;
-//        default: 
-//          user_error('Unsupported image type');
-//          return false;       
-//     }   
-//      
-//     if(!$img) 
-//     { 
-//        user_error("Unable to open image file"); 
-//        return false; 
-//     } 
-//     for($x = 0; $x < $size[0]; $x += $granularity) 
-//     { 
-//        for($y = 0; $y < $size[1]; $y += $granularity) 
-//        { 
-//           $thisColor = imagecolorat($img, $x, $y); 
-//           $rgb = imagecolorsforindex($img, $thisColor); 
-//           $red = round(round(($rgb['red'] / 0x33)) * 0x33); 
-//           $green = round(round(($rgb['green'] / 0x33)) * 0x33); 
-//           $blue = round(round(($rgb['blue'] / 0x33)) * 0x33); 
-//           $thisRGB = sprintf('%02X%02X%02X', $red, $green, $blue); 
-//           if(array_key_exists($thisRGB, $colors)) 
-//           { 
-//              $colors[$thisRGB]++; 
-//           } 
-//           else 
-//           { 
-//              $colors[$thisRGB] = 1; 
-//           } 
-//        } 
-//     } 
-//     arsort($colors); 
-//     return array_slice(array_keys($colors), 0, $numColors); 
-//  }
-
-function getImageColorPalette($name, $count = 10) {
-    $fmap = array(
-        'gif'  => 'imagecreatefromgif',
-        'png'  => 'imagecreatefrompng',
-        'jpg'  => 'imagecreatefromjpg',
-        'jpeg' => 'imagecreatefromjpg'
-    );
-    $path = pathinfo($name);
-    $colors = array();
-    try {
-        list($sw,$sh) = getimagesize($name);
-        $m = getimagesize($name);
-        $mime = $m['mime'];
-                
-        $ext = strtolower($path['extension']);
-        switch($mime){
+  function getImageColorPalette($imageFile, $numColors = 8, $granularity = 5) 
+  { 
+     $granularity = max(1, abs((int)$granularity)); 
+     $colors = array(); 
+     $size = @getimagesize($imageFile); 
+     if($size === false) 
+     { 
+        user_error("Unable to get image size data"); 
+        return false; 
+     }
+     
+     switch($size['mime']){
         case 'image/png':
-          $im = @imagecreatefrompng($name);
+          $img = @imagecreatefrompng($imageFile);
           break;
         case 'image/jpeg':
-          $im = @imagecreatefromjpeg($name);
+          $img = @imagecreatefromjpeg($imageFile);
           break;
         default: 
           user_error('Unsupported image type');
           return false;       
-     }
-    } catch (Exception $e) {
-        error_log('Error reading image: ' . $name . ' ' . $e->getMessage());
-        return $colors;
-    }
-     // build true color images that are at most 400x300
-    $w = min(400,$sw);
-    $h = min(300,$sh);
-    $tc = imagecreatetruecolor($w,$h);
-    $qi = imagecreatetruecolor($w,$h);
-    // render a true color representation of the source image
-    imagecopyresampled($tc,$im,0,0,0,0,$w,$h,$sw,$sh);
-    // make a copy
-    imagecopy($qi,$tc,0,0,0,0,$w,$h);
-    // smooth it
-    imagefilter($qi, IMG_FILTER_SMOOTH, 2048);
-    // pixelate it so we can loop through less pixels
-    imagefilter($qi, IMG_FILTER_PIXELATE, 10, 1);
-    // try to "quantize" it by creating a palette that is at least 4 times as big as what we need
-    imagetruecolortopalette($qi, false, $count * 8);
-    // tune the palette based on the true color image
-    imagecolormatch($tc,$qi);
-    // do a quick pixel scan
-    for($x = 0; $x < $w; $x+=10) {
-        for($y = 0; $y < $h; $y+=10) {
-            $c = imagecolorsforindex($qi, imagecolorat($qi, $x, $y));
-            array_push($colors, sprintf("#%02X%02X%02X", $c['red'], $c['green'], $c['blue']));
-        }
-    }
-    // count unique values
-    $dist = array_count_values($colors);
-    arsort($dist);
-    // take the $count most popular values
-    $colors = array_merge(array_slice($dist, 0, $count/2), array_slice($dist, count($dist) - ($count), $count));
-    $colors = array_keys($colors);
-    arsort($colors);
-    imagedestroy($tc);
-    imagedestroy($im);
-    return $colors;
-}
- 
+     }   
+      
+     if(!$img) 
+     { 
+        user_error("Unable to open image file"); 
+        return false; 
+     } 
+     for($x = 0; $x < $size[0]; $x += $granularity) 
+     { 
+        for($y = 0; $y < $size[1]; $y += $granularity) 
+        { 
+           $thisColor = imagecolorat($img, $x, $y); 
+           $rgb = imagecolorsforindex($img, $thisColor); 
+           $red = round(round(($rgb['red'] / 0x33)) * 0x33); 
+           $green = round(round(($rgb['green'] / 0x33)) * 0x33); 
+           $blue = round(round(($rgb['blue'] / 0x33)) * 0x33); 
+           $thisRGB = sprintf('%02X%02X%02X', $red, $green, $blue); 
+           if(array_key_exists($thisRGB, $colors)) 
+           { 
+              $colors[$thisRGB]++; 
+           } 
+           else 
+           { 
+              $colors[$thisRGB] = 1; 
+           } 
+        } 
+     } 
+     arsort($colors); 
+     return array_slice(array_keys($colors), 0, $numColors); 
+  } 
 }
 
 function getThumb($img){
