12cd85e4e6650ad6356d8cce51b56b325b8118e0
Marta Fernandes <marta.a.fernandes@gmail.com>
2012-01-02 13:56:31 +0000

    comentarios com imagem

--- application/views/tribble/view.php
+++ application/views/tribble/view.php
@@ -1,46 +1,55 @@
-<div class="tribble-container" >
-  <div class="tribble-user-info">
-    <p><span><img src="" width="42" height="42" /> </span><a href="/">user name</a></p>
-  </div>
-  <div class="tribble-img-container box"> <img src="<?=$tribble->image?>" /></div>
-  <div class="tribble-img-data">
-    <h2>
-      <?=$tribble->title?>
-    </h2>
-    <div class="tribble-date">
-      <?=strftime('%B %d, %Y',mysql_to_unix($tribble->ts));?>
+<div class="g75">
+  <div class="tribble-container">
+    <div class="tribble-user-info"> <a href="/" title="Kubilay Sapayer"><img src="http://tribble.local/data/b5e0eaebec229148d61d1881b27d1865e1bb5003/dispic2_thumb.jpg" alt="Logobig" width="54" height="54"/></a>
+      <div class="tribble-title">
+        <h2>twitter app iOS</h2>
+      </div>
+      <h4><a href="/">Username</a></h4>
+      <div class="tribble-date">aasdasd</div>
     </div>
-    <div class="tribble-desc">
-      <?=$tribble->text?>
+    <div class="tribble-img-container box clear"> <img src="" width="400" height="300"/></div>
+    <div class="tribble-img-data">
+      <div class="tribble-desc">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      <div class="tribble-tools">
+        <p class="ico"><a href="/" class="likes">20</a></p>
+      </div>
+      <h3>Tags</h3>
+      <ul class="tags">
+        <li><a href="/">jfsdfsdfsfshkh</a></li>
+        <li><a href="/">jhsdfsdkh</a></li>
+        <li><a href="/">jhksfh</a></li>
+        <li><a href="/">jhsdfsdkh</a></li>
+      </ul>
+      <h3>Color Scheme</h3>
+      <ul class="color-scheme">
+        <li><a href="/">jfsdfsdfsfshkh</a></li>
+        <li><a href="/">jhsdfsdkh</a></li>
+        <li><a href="/">jhksfh</a></li>
+        <li><a href="/">jhsdfsdkh</a></li>
+      </ul>
     </div>
-    <div class="tribble-tools">
-      <p class="ico"><a href="" class="likes">
-        <?=$tribble->likes?></a>likes
-      </p>
+    <div class="comments-list">
+      <h3>COMENTARIOS</h3>
+      <ul id="comments">
+        <li class="response">
+          <h4><a href="/" class="url" rel="contact" title="Luis Cavaco"><img src="http://tribble.local/data/b5e0eaebec229148d61d1881b27d1865e1bb5003/dispic2_thumb.jpg" alt="Me" width="42" height="42" class="photo fn" />Username</a> </h4>
+          <div class="comment-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<a href="/"> Ut enim ad minim veniam, </a>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+          <p class="comment-date">10 days ago</p>
+        </li>
+        <li class="response">
+          <h4><a href="/" class="url" rel="contact" title="Luis Cavaco"><img src="http://tribble.local/data/b5e0eaebec229148d61d1881b27d1865e1bb5003/dispic2_thumb.jpg" alt="Me" width="42" height="42" class="photo fn" />Username</a> </h4>
+          <div class="comment-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p class="comment-body-image"> <img src="http://tribble.local/data/b5e0eaebec229148d61d1881b27d1865e1bb5003/dispic2_thumb.jpg" alt="Me" width="378" height="282" class="photo fn" /> </p>
+            <p> Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+          <p class="comment-date">10 days ago</p>
+        </li>
+      </ul>
     </div>
-    <h3>Tags</h3>
-    <ul class="tags">
-      <? $tags = explode(',',$tribble->tags); ?>
-      <?foreach($tags as $tag):?>
-      <li><a href="#">
-        <?=$tag?>
-        </a></li>
-      <?endforeach?>
-    </ul>
-    <h3>Color Scheme</h3>
-    <ul class="color-scheme">
-      <?foreach(json_decode($tribble->palette) as $color):?>
-      <li style="background: #<?=$color?>;">&nbsp;</li>
-      <?endforeach;?>
-    </ul>
-  </div>
-  <div class="comments-list">
-  <h3>It seems no one has replied to this tribble</h3>
-  <ul>
-  <?if(!$replies):?>
-<li>
-</li>
-  <?endif;?>
-  </ul>
   </div>
 </div>
