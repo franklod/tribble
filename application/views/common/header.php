9e5098575e1db8720ed5480b3d431f47def74139
Marta Fernandes <marta.a.fernandes@gmail.com>
2012-01-02 19:33:04 +0000

    rebound comments

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -3,10 +3,10 @@
 <ul id="login">
 <?if(!$this->session->userdata('uid')):?>
   <li class="sign"><a href="<?=site_url()."/user/signup"?>">Sign up</a></li>
-  <li class="log btn"><a href="<?=site_url()."/auth/login"?>">Login</a></li>
+  <li class="log"><a href="<?=site_url()."/auth/login"?>" class="btn">Login</a></li>
 <?else:?>
   <li class="sign"><a href="<?=site_url()."/user/profile"?>"><?=$user->user_realname?></a></li>
-  <li class="log btn"><a href="<?=site_url()."/auth/logout/".uri_to_string(uri_string())?>">Logout</a></li>
+  <li class="log"><a href="<?=site_url()."/auth/logout/".uri_to_string(uri_string())?>" class="btn">Logout</a></li>
 <?endif;?>
 </ul>
 </div>
