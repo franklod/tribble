af31710d51c4ff03cbab9c1e55d67a6429882c4a
Pedro Correia <pedro.a.correia@co.sapo.pt>
2012-01-03 18:23:42 +0000

    logo provisorio

--- application/views/tribble/replyform.php
+++ application/views/tribble/replyform.php
@@ -11,27 +11,14 @@
   </div>
   <? endif;?>
   
-  <?=form_open_multipart('/tribble/reply/'.$tribble->id)?>
+  <?=form_open_multipart('/tribble/comment/'.$tribble->id)?>
   <?=form_fieldset('')?>
-  <div class="e_wrap">
-  <?=form_label('Image','image')?>
-  <?=form_upload(array('name'=>'image','id'=>'image'))?>
-  <p class="note"><strong>Note:</strong> image size is restricted to 400 x 300 pixels.</p>
+    <div class="e_wrap">
+  <?=form_label('Comment','text')?>
+  <?=form_textarea(array('name'=>'text','id'=>'text','placeholder'=>''))?>
   </div>
   <div class="e_wrap">
-  <?=form_label('Title','title')?>
-  <?=form_input(array('name'=>'title','id'=>'title','placeholder'=>'Give your work a nice title'))?>
-  </div>
-  <div class="e_wrap">
-  <?=form_label('Description','text')?>
-  <?=form_textarea(array('name'=>'text','id'=>'text','placeholder'=>'Write a short comment or description about your work'))?>
-  </div>
-  <div class="e_wrap">
-  <?=form_label('Tags','tags')?>
-  <?=form_input(array('name'=>'tags','id'=>'tags','placeholder'=>'Use comma separated expressions to tag your work'))?>
-  </div>
-  <div class="e_wrap">
-  <?=form_submit(array('name'=>'reply','id'=>'reply','value'=>'Reply','class'=>'btn_success'))?>
+  <?=form_submit(array('name'=>'reply','id'=>'reply','value'=>'Comment','class'=>'btn_success'))?>
   </div>
   <?=form_fieldset_close()?>
   <?=form_close()?>
