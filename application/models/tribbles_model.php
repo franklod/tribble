b4c12fe90bd6f7c3543d6659e434b55e1aebd733
Pedro Correia <pedro.a.correia@co.sapo.pt>
2012-01-03 15:42:02 +0000

    Add the replies db stuff and to the view.php file

--- application/models/tribbles_model.php
+++ application/models/tribbles_model.php
@@ -202,10 +202,51 @@ class Tribbles_model extends CI_Model {
     }
     
     function getReplies($tribble_id,$currentPage=0){
-      $repliesPerPage = 10;
-      $page = $repliesPerPage * $currentPage;
-      //$query = $this->db->get_where('tribbles','tribble_parent_id = '.$tribble_id,$repliesPerPage,$page);
-      //return $query->result();            
+      /*
+      SELECT 
+        tr_tribbles.tribble_text,
+        tr_tribbles.tribble_title,
+        tr_comments.comment_text,
+        tr_replies_ref.ref_timestamp,
+        tr_users.user_realname AS com_username,
+        tr_users.user_id AS com_userid,
+        tr_users1.user_realname AS reb_username,
+        tr_users1.user_id AS reb_userid,
+        tr_images.image_path AS image
+      FROM
+        tr_replies_ref
+        LEFT OUTER JOIN tr_comments ON (tr_replies_ref.ref_comment_id = tr_comments.comment_id)
+        LEFT OUTER JOIN tr_tribbles ON (tr_replies_ref.ref_rebound_id = tr_tribbles.tribble_id)
+        LEFT OUTER JOIN tr_users ON (tr_comments.comment_user_id = tr_users.user_id)
+        LEFT OUTER JOIN tr_users tr_users1 ON (tr_tribbles.tribble_user_id = tr_users1.user_id)
+        LEFT OUTER JOIN tr_images ON (tr_tribbles.tribble_user_id = tr_images.image_tribble_id)
+      WHERE
+        tr_replies_ref.ref_tribble_id = 54
+      */
+      
+      $this->db->select('
+        tr_tribbles.tribble_text AS reb_text,
+        tr_tribbles.tribble_title AS reb_title,
+        tr_comments.comment_text,
+        tr_replies_ref.ref_timestamp as ts,
+        tr_users.user_realname AS com_username,
+        tr_users.user_id AS com_userid,
+        tr_users1.user_realname AS reb_username,
+        tr_users1.user_id AS reb_userid,
+        tr_images.image_path AS image,
+        tr_tribbles.tribble_id AS reb_id
+      ');
+      $this->db->from('tr_replies_ref');
+      $this->db->join('tr_comments','tr_replies_ref.ref_comment_id = tr_comments.comment_id','LEFT OUTER');
+      $this->db->join('tr_tribbles','tr_replies_ref.ref_rebound_id = tr_tribbles.tribble_id','LEFT OUTER');
+      $this->db->join('tr_users','tr_comments.comment_user_id = tr_users.user_id','LEFT OUTER');
+      $this->db->join('tr_users tr_users1','tr_tribbles.tribble_user_id = tr_users1.user_id','LEFT OUTER');
+      $this->db->join('tr_images','tr_tribbles.tribble_id = tr_images.image_tribble_id','LEFT OUTER');
+      $this->db->where(array('tr_replies_ref.ref_tribble_id'=>$tribble_id));
+      
+      $query = $this->db->get();
+      $result = $query->result();
+      return $result;                  
     }
     
     function addLike($tribble_id,$uid){
