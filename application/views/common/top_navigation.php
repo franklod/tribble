90c7cb9918bf1712ce0d87a669fa618fb1368709
Marta Fernandes <marta.a.fernandes@gmail.com>
2012-01-04 17:44:56 +0000

    ui, tanta coisa

--- application/views/common/top_navigation.php
+++ application/views/common/top_navigation.php
@@ -11,17 +11,31 @@ if($this->session->userdata('uid')){
 }
 
 ?>
+
 <div id="navContainer">
-<div id="topNavigation" class="blackMenu">
-<ul class="h_navigation">
-<? foreach($pages as $page): ?>
-  <? if(current_url() == site_url()."/".$page->uri): ?>    
-  <li class="active"><a href="<?=site_url()."/".$page->uri?>"><?=$page->text?></a></li>
-  <? else: ?>
-  <li><a href="<?=site_url()."/".$page->uri?>"><?=$page->text?></a></li>
-  <? endif; ?>
-<? endforeach; ?>
-</ul>
-</div>
+  <div id="topNavigation" class="blackMenu">
+    <ul class="h_navigation">
+      <? foreach($pages as $page): ?>
+      <? if(current_url() == site_url()."/".$page->uri): ?>
+      <li class="active"><a href="<?=site_url()."/".$page->uri?>">
+        <?=$page->text?>
+        </a></li>
+      <? else: ?>
+      <li><a href="<?=site_url()."/".$page->uri?>">
+        <?=$page->text?>
+        </a></li>
+      <? endif; ?>
+      <? endforeach; ?>
+    </ul>
+    <form id="search">
+      <input name="searchInput" type="text" class="" id="searchInput" placeholder="Pesquisar" />
+    </form>
+    <div id="login">
+      <ul >
+        <li><a href="/">Sign In</a></li>
+        <li><a href="/">Log In</a></li>
+      </ul>
+    </div>
+  </div>
 </div>
 <div id="main">
