ac68c1affeaf96eb6147e90475cb9425155fd356
Pedro Correia <pedro.a.correia@co.sapo.pt>
2012-01-05 11:38:19 +0000

    adds the REST API thingies and some memcached stuff

--- api.php
+++ api.php
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'application/api';
+	$application_folder = 'application/api/';
 
 /*
  * --------------------------------------------------------------------
--- application/api/views/common/page_top.php
+++
@@ -1,5 +0,0 @@
-<?
-  $this->load->view('common/page_start');
-  $this->load->view('common/header');
-  $this->load->view('common/top_navigation');
-?>
