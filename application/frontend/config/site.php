9cfe3ec0a12b3a933aea844522a49dcbf9ffb7b6
Pedro Correia <pedro.a.correia@co.sapo.pt>
2012-02-01 17:52:14 +0000

    blip

--- application/frontend/config/site.php
+++ application/frontend/config/site.php
@@ -1,5 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['site_name'] = 'GAB';
+$config['site_name'] = 'Tribble';
 $config['site_description'] = 'A tool for design work sharing and discussion.';
 $config['site_keywords'] = 'design,work,';
